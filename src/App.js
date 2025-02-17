4ff97998b449b43ce73cb9b4af04ad581994bbac
PierrePivz <chillingd@macbook-pro-de-pierre.home>
2025-02-17 15:59:57 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Shelve from "./Shelve.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <div class="shelves_container">
+          <div class="row" id="text">
+            <p>select a shelve and start diggin :</p>
+            <p class="name_display" id="house_display">HOUSE</p><p class="name_display" id="techno_display">TECHNO</p><p class="name_display" id="acidjazz_display">ACID-JAZZ</p>
+            <p class="name_display" id="hiphouse_display">HIP-HOUSE</p><p class="name_display" id="indierock_display">INDIE ROCK</p><p class="name_display" id="house2011_now_display">HOUSE 2011 NOW</p>
+            <p class="name_display" id="italohouse_display">ITALO HOUSE</p>
+            <p>or browse through catalogue</p>
+
+          </div>
+          <div class="row" id="selection">
+
+
+          
+
+          <Shelve />
+          <Shelve />
+          <Shelve />
+          <Shelve />
+          <Shelve />
+          <Shelve />
+          <Shelve />
+          <Shelve />
+
+          
+
+
+
+
+
+         
+          
+          
+          </div>
+        </div>
+        
+          
       </header>
     </div>
   );
---
+++ src/Shelve.js
@@ -0,0 +1,60 @@
+
+
+
+export default function Shelve(){
+
+
+    return(
+        <div class="shelve_block">
+          <div class="shelve" id="acid_jazz" >
+            
+          </div>
+          <div class="shelve" id="techno">
+            
+          </div>
+          <div class="shelve" id="italo_house">
+            
+          </div>
+          <div class="shelve" id="hip-house">
+            
+          </div>
+          <div class="shelve" id="house2011_now">
+            
+          </div>
+          <div class="shelve" id="indie">
+            
+          </div>
+          <div class="shelve" id="house">
+            
+          </div>
+          <div class="shelve" id="house">
+            
+          </div>
+          <div class="shelve" id="house" >
+            
+          </div>
+          <div class="shelve" id="techno">
+            
+          </div>
+          <div class="shelve" id="italo_house">
+            
+          </div>
+          <div class="shelve" id="house">
+            
+          </div>
+          <div class="shelve" id="house">
+            
+          </div>
+          <div class="shelve" id="house">
+            
+          </div>
+          <div class="shelve" id="house">
+            
+          </div>
+          <div class="shelve" id="house">
+            
+          </div>
+          
+          </div>
+    );
+}
