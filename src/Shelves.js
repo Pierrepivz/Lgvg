02bc35f277c340ccc857512448230f8291691286
PierrePivz <chillingd@macbook-pro-de-pierre.home>
2025-02-28 17:00:05 +0100

    new

--- src/Shelves.js
+++ src/Shelves.js
@@ -6,14 +6,18 @@ export default function Shelves(){
 
           <div class="row" id="text">
             <p id="intro">"Enter the diggin portal .... your introduction phrase" </p>
-            <p class="name_display" id="house_display">HOUSE</p><p class="name_display" id="techno_display">TECHNO</p><p class="name_display" id="acidjazz_display">ACID-JAZZ</p>
+            
+            <button>Our distribution (for record shops)</button>
+          </div>
+          <div class="shelve_presentation">
+           
+          <p class="name_display" id="house_display">HOUSE</p><p class="name_display" id="techno_display">TECHNO</p><p class="name_display" id="acidjazz_display">ACID-JAZZ</p>
             <p class="name_display" id="hiphouse_display">HIP-HOUSE</p><p class="name_display" id="indierock_display">INDIE ROCK</p><p class="name_display" id="house2011_now_display">HOUSE 2011 NOW</p>
             <p class="name_display" id="italohouse_display">ITALO HOUSE</p>
-            <button>Our distrubition (for record shops)</button>
-          </div>
-          <div class="description">
-            <p class="name_display" id="house_description">progressive house description</p>
-          </div>
+            <div class="description">
+            <p class="name_display" id="house_description">Dérivé de la House et ancêtre de la Trance. Ce genre voit le jour à Chicago dans les années 1980 et se caractérise par l’utilisation d’une basse analogique créée à partir du synthétiseur-séquenceur Roland TB-303.
+            </p>
+          </div></div> 
 
           <div class="row" id="selection">
 
@@ -28,6 +32,7 @@ export default function Shelves(){
           <Shelve />
           <Shelve />
           <Shelve />
+          
 
           
 
